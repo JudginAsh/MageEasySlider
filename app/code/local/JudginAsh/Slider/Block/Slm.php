37adc7462e08f05f55becaee67e2f85e223e1880
Evgenii Ashytok <webxakep@i.ua>
2017-02-23 14:04:38 +0200

    first commit

---
+++ app/code/local/JudginAsh/Slider/Block/Slm.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 22.02.2017
+ * Time: 16:39
+ */
+class JudginAsh_Slider_Block_Slm extends Mage_Core_Block_Template
+{
+    public function getSlider()
+    {
+        return true;
+    }
+}
---
+++ app/code/local/JudginAsh/Slider/Helper/Data.php
@@ -0,0 +1,7 @@
+<?php
+
+class JudginAsh_Slider_Helper_Data extends Mage_Core_Helper_Abstract
+{
+
+}
+	 
